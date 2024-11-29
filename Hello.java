758414d9c57bc1aad20828a273ea8c03637c061c
Mtagintsev <mtagintsev@csus.edu>
2024-11-29 13:27:31 -0800

    Matt's commit

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("303889157 - Jesus Betancourt");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("304318534 - Matthew Tagintsev");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("303343209 - Hunter Powell");
 		System.out.println("SacStateID - This line was added by First name Last name.");
